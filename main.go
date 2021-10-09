a1a98ad37e85716a5aa9b63e93faa0a2021457b4
Anant chauhan <63709820+Anant1711@users.noreply.github.com>
2021-10-09 23:27:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"net/http" 	//Package http provides HTTP client and server implementations.
 	"log"
 	"encoding/json"		//Package json implements encoding and decoding of JSON
 	"time"
-
 )
-
 
 type UData struct{
 	ID string `json:"ID"`
@@ -29,37 +27,74 @@ type PData struct{
 type Daata []UData 		//array for user data
 type postDaata []PData  //array for Post Data
 
+
+
 func userData(w http.ResponseWriter, r *http.Request){
+	n1:="Anant"
+	n2:="Test"
 	data:=Daata{
-		UData{ID:"001", Name:"anant", Email:"anantExample@gmail.com", Password:"1234@"},
+		UData{ID:"001", Name:n1, Email:"anantExample@gmail.com", Password:"1234@"},UData{ID:"002", Name:n2, Email:"TestExample@gmail.com", Password:"Test@"},
 	}
-	
 	json.NewEncoder(w).Encode(data)
+
+	keys, ok := r.URL.Query()["id"]
+        if !ok || len(keys[0]) < 1 {
+            log.Println("Url Param 'id' is missing")
+            return
+        }
+    	key := keys[0]
+		fmt.Println(key)
+		switch k:=key; k{
+		case "1":
+			fmt.Println("Password: 1234@")
+			w.Write([]byte(n1))
+		case "2":
+			fmt.Println("Password: Test@")
+			w.Write([]byte(n2))
+
+		}
+		
 }
 
 func postsData(w http.ResponseWriter, r *http.Request){
 	now:=time.Now()
+	id1,id2:=001,002
+	c1:="Hey myslef Anant, this is my First Post"
+	c2:="Hey myslef Test, this is my First Post"
 	p_data:=postDaata{
-		PData{ID:"001", Caption:"Hey, its my First Post", Image_url:"abcd",Stamp:now},
+		PData{ID:"001", Caption:c1, Image_url:"abcd.com",Stamp:now},PData{ID:"002", Caption:c2, Image_url:"efgh.com",Stamp:now},
 	}
 	json.NewEncoder(w).Encode(p_data)
-}
 
+	keys, ok := r.URL.Query()["id"]
+        if !ok || len(keys[0]) < 1 {
+            log.Println("Url Param 'id' is missing")
+            return
+        }
+    	key := keys[0]
+		fmt.Println(key)
+		switch k:=key; k{
+		case "1":
+			fmt.Println(id1)
+			w.Write([]byte(c1))
+		case "2":
+			fmt.Println(id2)
+			w.Write([]byte(c2))
+
+		}
+}
 
 func homepage(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("<h1>Welcome to Instagram</h1>"))
 }
 
-
 func HandlereRequest(){
 	http.HandleFunc("/", homepage)
-	http.HandleFunc("/users", userData)
-	http.HandleFunc("/posts", postsData)
-	
+	http.HandleFunc("/users/", userData)
+	http.HandleFunc("/posts/", postsData)
 	log.Fatal(http.ListenAndServe(":3377", nil))
 }
-
-
 func main(){
 	HandlereRequest()
+	
 }
